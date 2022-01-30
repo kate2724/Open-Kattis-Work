4b5f64b51c2ccdfd041e7fcfd91bdf1847fee3d9
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-01-30 14:52:00 -0600

    finished

---
+++ fiftyShadesOfPink.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class fiftyShadesOfPink {
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in);
+        int buttons = Integer.parseInt(sc.nextLine());
+        int buttonCounter=0;
+        for (int i = 0; i < buttons; i++) {
+            String buttonColor = sc.nextLine().toLowerCase(); 
+            if(buttonColor.contains("pink")|| buttonColor.contains("rose")){
+                buttonCounter++;
+            }           
+        }
+        if(buttonCounter>0){
+            System.out.println(buttonCounter);
+        }
+        else{
+            System.out.println("I must watch Star Wars with my daughter");
+        }
+
+    }
+}

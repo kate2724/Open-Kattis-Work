da6d4ec17d14c88829dfdb6b421f69353d4be114
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-01-30 13:59:32 -0600

    basic structure

--- qalyProblem.java
+++ qalyProblem.java
@@ -1,13 +1,21 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 
 
 public class qalyProblem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-
+        int periods = Integer.parseInt(sc.nextLine());
+        System.out.println("period: "+ periods);
+        float total=0;
+        float product=0;
+        while (sc.hasNext()) {
+            float qaly = sc.nextFloat();
+            float years = sc.nextFloat();
+            product = qaly + years;
+            total = total + product;
+        }
+        System.out.println("total" + total);
+        sc.close();
     }
 }
 

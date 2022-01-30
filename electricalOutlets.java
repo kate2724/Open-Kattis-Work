a795d2418431754c8f8313e004430d68af8cecef
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-01-30 14:27:41 -0600

    setup

---
+++ electricalOutlets.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class electricalOutlets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int periods = Integer.parseInt(sc.nextLine());
+    }
+}

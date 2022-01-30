bd45342185e1d5c972565401f449b006a9237f76
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-01-30 14:41:12 -0600

    finished

--- electricalOutlets.java
+++ electricalOutlets.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class electricalOutlets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int periods = Integer.parseInt(sc.nextLine());
+        int testCases = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < testCases; i++) {
+            int powerStrips=sc.nextInt();
+            int maxOutlets=1;
+            for (int j = 0; j < powerStrips; j++) {
+                maxOutlets = maxOutlets + sc.nextInt() - 1;
+            }
+            System.out.println(maxOutlets);
+
+        }
+
+
     }
 }

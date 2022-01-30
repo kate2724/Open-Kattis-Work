e2573731250369a5f0d8d15dba6680cc64b27a91
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-01-30 13:32:44 -0600

    Set up workspace

---
+++ qalyProblem.java
@@ -0,0 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+
+public class qalyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+    }
+}
+

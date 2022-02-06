8feeecb7216021b97160643ab498675cc7e2de53
Katelyn Breen <60229563+kate2724@users.noreply.github.com>
2022-02-06 15:26:41 -0600

    finished problem

--- socialRunning.java
+++ socialRunning.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class socialRunning {
-    public static void main(String[] args) { 
+    public static void main(String[] args) throws FileNotFoundException { 
+        // File file = new File("test.txt");
+        // Scanner sc = new Scanner(file);
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        int minimumNumber=(10^4)*2;
+        int minimumNumber=100000000;
         ArrayList<Integer> listOfDistances = new ArrayList<Integer>();
         while(sc.hasNext()){
             int nextDistance = Integer.parseInt(sc.nextLine());
             listOfDistances.add(nextDistance);
         }
-        for (int i = 0; i < listOfDistances.size()-1; i++) {
-            if(listOfDistances.get(i)+listOfDistances.get(n-2)<minimumNumber){
-                minimumNumber=listOfDistances.get(i)+listOfDistances.get(n-2);
+        sc.close();
+        for (int i = 0; i < n; i++) {
+            int finalDistanceIndex = i+ n-2;
+            while(finalDistanceIndex>n-1){
+                finalDistanceIndex=finalDistanceIndex-n;
+            }
+            if(listOfDistances.get(i)+listOfDistances.get(finalDistanceIndex) < minimumNumber){
+                minimumNumber=listOfDistances.get(i)+listOfDistances.get(finalDistanceIndex);
             }
         }
         System.out.println(minimumNumber);
-        sc.close();
     }
 }
